8a32833361098f516fb68adb63fbf09489631502
becem gharbi <becem.gharbi@live.com>
2023-07-06 12:22:34 +0100

    feat: Create landing page

---
+++ components/Logo.vue
@@ -0,0 +1,4 @@
+<template>
+    <img v-if="$colorMode.value === 'light'" src="~/assets/logo/light.svg" alt="logo">
+    <img v-else src="~/assets/logo/dark.svg" alt="logo">
+</template>
---
+++ components/content/Hero/Index.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container">
+
+        <div>
+
+            <p class="title">
+                ESP Admin
+                <Badge>Alpha</Badge>
+            </p>
+
+            <p class="description">Remote management for ESP microcontrollers</p>
+
+            <slot name="extra"></slot>
+
+            <div class="actions">
+
+                <ButtonLink href="https://esp-admin.bg-tech.tn/auth/login" color="blue">
+                    Get started
+                </ButtonLink>
+
+                <a href="/getting_started"
+                    class="py-px mt-px font-medium text-gray-800 border-b-2 border-transparent dark:text-gray-100 hover:border-gray-800 dark:hover:border-gray-100">
+                    Documentation
+                </a>
+            </div>
+        </div>
+
+        <div class="graphic">
+            <Graphic />
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+.container {
+    margin: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.actions {
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+    margin-top: 2rem;
+}
+
+.title {
+    display: flex;
+    gap: 1rem;
+    font-size: x-large;
+    font-weight: 500;
+    margin-bottom: 0.5rem;
+}
+
+.description {
+    font-size: large;
+    color: gray;
+    margin-bottom: 2rem;
+}
+
+@media screen and (max-width: 768px) {
+    .container {
+        margin: 2rem 1rem;
+    }
+
+    .title {
+        font-size: large;
+    }
+
+    .description {
+        font-size: medium;
+    }
+
+    .graphic {
+        display: none;
+    }
+}
+</style>
