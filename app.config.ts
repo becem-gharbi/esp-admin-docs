1a80bb4230f5269016aadd75241f85aaa81953da
becem gharbi <becem.gharbi@live.com>
2023-07-11 19:10:00 +0100

    chore: Set social image

--- app.config.ts
+++ app.config.ts
@@ -2,23 +2,10 @@ export default defineAppConfig({
   docus: {
     title: "ESP Admin",
     description: "Remote management for ESP microcontrollers",
-    // image:
-    //   "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
+    image: "/poster.png",
     socials: {
       github: "becem-gharbi/esp-admin",
-      // nuxt: {
-      //   label: "Nuxt",
-      //   icon: "simple-icons:nuxtdotjs",
-      //   href: "https://nuxt.com",
-      // },
     },
-    // github: {
-    //   dir: ".starters/default/content",
-    //   branch: "main",
-    //   repo: "docus",
-    //   owner: "nuxt-themes",
-    //   edit: true,
-    // },
     aside: {
       level: 0,
       collapsed: false,
