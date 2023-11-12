a3f6bb699723d25575026388a2bdb7b1373a1aa4
becem-gharbi <becem.gharbi@live.com>
2023-11-12 16:35:45 +0100

    chore: Remove RC badge

--- components/content/Hero/Index.vue
+++ components/content/Hero/Index.vue
@@ -1,34 +1,37 @@
 <template>
-    <div class="container">
+  <div class="container">
+    <div>
+      <p class="title">
+        ESP Admin
+        <Badge type="success">
+          Stable
+        </Badge>
+      </p>
 
-        <div>
-            <p class="title">
-                ESP Admin
-                <Badge type="success">RC</Badge>
-            </p>
+      <p class="description">
+        Remote management for ESP microcontrollers
+      </p>
 
-            <p class="description">Remote management for ESP microcontrollers</p>
+      <slot name="extra" />
 
-            <slot name="extra"></slot>
+      <div class="actions">
+        <ButtonLink href="https://app.esp-admin.tn" color="blue">
+          Get started
+        </ButtonLink>
 
-            <div class="actions">
-
-                <ButtonLink href="https://app.esp-admin.tn" color="blue">
-                    Get started
-                </ButtonLink>
-
-                <NuxtLink to="/getting_started"
-                    class="py-px mt-px font-medium text-gray-800 border-b-2 border-transparent dark:text-gray-100 hover:border-gray-800 dark:hover:border-gray-100">
-                    Documentation
-                </NuxtLink>
-            </div>
-        </div>
-
-        <div class="graphic">
-            <Graphic />
-        </div>
-
+        <NuxtLink
+          to="/getting_started"
+          class="py-px mt-px font-medium text-gray-800 border-b-2 border-transparent dark:text-gray-100 hover:border-gray-800 dark:hover:border-gray-100"
+        >
+          Documentation
+        </NuxtLink>
+      </div>
     </div>
+
+    <div class="graphic">
+      <Graphic />
+    </div>
+  </div>
 </template>
 
 <style scoped>
